bbdf0db559a7ca359997cfa3b1b6ad152405e5da
NuggetAigri <johann.caron@outlook.fr>
2026-07-25 20:51:40 +0200

    feat: Add channels and messages

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
-use crate::models::{User, LoginState};
+use crate::models::{Channel, LoginState, User};
 
 pub mod models;
+pub mod test;
 
 fn main() {
     let user1 = User::new(1, String::from("Alice"));
     show_status(&user1, LoginState::LoggedIn);
-    show_status(&user1, LoginState::Waiting(String::from("Vérification du pair...")));
+    show_status(
+        &user1,
+        LoginState::Waiting(String::from("Vérification du pair...")),
+    );
 
+    let mut channel = Channel::new(0, "general");
+    channel.post_message(user1.id(), "Petit test 1");
+    channel.post_message(user1.id(), "Petit test2");
+    channel.post_message(user1.id(), "Petit test3");
+
+    for message in channel.history() {
+        println!("{message}");
+    }
 }
 
 fn show_status(user: &User, state: LoginState) {
-    println!( "{} (#{}) - {}", user.username(), user.id(), state.message())
-} 
+    println!("{} (#{}) - {}", user.username(), user.id(), state.message())
+}
--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,6 @@
+use std::time::SystemTime;
+use std::fmt::Display;
+
 pub struct User {
     id: u64,
     username: String,
@@ -34,3 +37,49 @@ impl LoginState {
         }
     }
 }
+
+pub struct Channel {
+    id: u64, 
+    name: String,
+    messages: Vec<Message>
+}
+
+impl Channel {
+    pub fn new(id: u64, name: &str) -> Channel {
+        Channel { id, name: String::from(name), messages: Vec::new() } 
+    }
+
+    pub fn post_message(&mut self, author_id: u64, content: &str) {
+        let message = Message::new(author_id, content);
+        self.messages.push(message);
+    }
+
+    pub fn history(&self) -> &[Message] {
+        &self.messages
+    }
+}
+
+pub struct Message {
+    author_id: u64,
+    content: String,
+    timestamp: u64,
+}
+
+impl Message {
+    pub fn new(author_id: u64, content: &str) -> Message {
+        let timestamp = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .expect("Clock may run backward")
+        .as_secs();
+
+        Message { author_id, content: String::from(content), timestamp }
+    }
+}
+
+impl Display for Message {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} (timestamp: {})", self.content, self.timestamp)
+    }
+}
+
+
