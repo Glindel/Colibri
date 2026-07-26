548854a1ce064a3f1421eef5ccb3d9258dc1ae45
NuggetAigri <johann.caron@outlook.fr>
2026-07-26 20:02:08 +0200

    feat: Learn mpsc on tokio

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use crate::models::{Channel, LoginState, User};
+use std::time::Duration;
+
+use crate::models::{Channel, LoginState, User, Message};
+use tokio::sync::mpsc;
+use tokio::sync::mpsc::{Sender, Receiver};
 
 pub mod models;
-pub mod test;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let user1 = User::new(1, String::from("Alice"));
     show_status(&user1, LoginState::LoggedIn);
     show_status(
@@ -11,14 +15,24 @@ fn main() {
         LoginState::Waiting(String::from("Vérification du pair...")),
     );
 
-    let mut channel = Channel::new(0, "general");
-    channel.post_message(user1.id(), "Petit test 1");
-    channel.post_message(user1.id(), "Petit test2");
-    channel.post_message(user1.id(), "Petit test3");
+    let (sender, mut receiver): (Sender<Message>, Receiver<Message>) = mpsc::channel(100);
 
-    for message in channel.history() {
+
+    tokio::spawn(async move {
+       loop {
+            tokio::time::sleep(Duration::from_secs(1)).await;
+            let message = Message::new(user1.id(), "Ceci est un petit ping");
+            if let Err(_) = sender.send(message).await {
+                println!("Receiver dropped");
+            }
+        }
+    });
+
+    while let Some(message) = receiver.recv().await {
         println!("{message}");
     }
+
+
 }
 
 fn show_status(user: &User, state: LoginState) {
